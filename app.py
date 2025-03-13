850050c30a4dc795634c1d2405e56a296ba14acd
Dan Douglas <dandoug@gmail.com>
2025-03-12 22:12:36 -0700

    initialize environment variables during app init.  include skeleton checked into source control for documentation purposes

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()  # Loads variables from .env into the environment
 
 app = Flask(__name__)
-
+RDS_PORT = os.getenv('RDS_PORT')
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return f"Hello World!  {RDS_PORT}"
 
 
 if __name__ == '__main__':
